64b8ebe17c72778e6421bbf1ef70bd9d86cdde16
Flynn <frh949@gmail.com>
2017-03-16 13:19:23 +1100

    Shifted table of critical values to a separate file

--- beadpy/__init__.py
+++ beadpy/__init__.py
@@ -3,3 +3,5 @@ from .resultsprocessor import drift_subtractor, unit_converter, spurious_removal
 from .changepoint import lsq, ss2lines, loglik, changePoint, linefit, binary_search, segment_finder,confidenceThreshold
 
 from .analysisandplots import segmentplotter, filterer, trajectory_plotter, weighted_avg_and_std, ratefinder, ratefinder_autosigma, ratehist, processivity, sigmaval_finder
+
+from .criticalvals import confidencevals
